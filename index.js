10f6bf25513e0237919c3b76f245820c14c326a5
John-thabuks <thabuksjohn@gmail.com>
2023-09-28 17:33:29 +0300

    Assignment completed.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(defaultString = "roller-skate"){
+    return `This Saturday, I want to ${defaultString}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function saturdayFun(defaultString = 'roller-skate'){
+    return `This Saturday, I want to ${defaultString}!`;
+}
+
+function mondayWork(defaultString = 'go to the office'){
+    return `This Monday, I will ${defaultString}.`;
+}
+
+function wrapAdjective(dafaultString = "*"){
+    return function (defaultSpecial = "special"){
+        return `You are ${dafaultString}${defaultSpecial}${dafaultString}!` 
+    }
+}
